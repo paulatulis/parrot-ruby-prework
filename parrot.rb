dde101a6d840c2b6745eef5f1a1d5b5c48cb2544
Paula Tulis <tulis.paula@gmail.com>
2019-02-26 01:48:58 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(sound = "Squawk!")
-  return sound
-  puts sound
+  return "#{sound}"
+  puts "#{sound}"
 end
 
 parrot(sound)
